8e70cf5ef3a6f59d4fe6e4f42148829924f4fe7b
Muhammad Humza Ghayas <humza.ghayas@royalcyber.com>
2023-01-20 19:29:13 +0500

    initial commit

---
+++ tcs_ct_customization/packages/b2b-terraform/default/provider.tf
@@ -0,0 +1,8 @@
+provider "commercetools" {
+  client_id     = "YKOf4i-uVrPzW_rJumZgPXyF"
+  client_secret = "27mPnpsDKsswAdjmnpBFWEL82bYm1P-h"
+  project_key   = "tcs-test-project"
+  scopes        = "manage_project:tcs-test-project"
+  token_url     = "https://auth.us-central1.gcp.commercetools.com"
+  api_url       = "https://api.us-central1.gcp.commercetools.com"
+}
