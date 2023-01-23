d890818bdbeab70c53f5715fc1d2bb1953c100f1
Muhammad Humza Ghayas <humza.ghayas@royalcyber.com>
2023-01-23 21:17:54 +0500

    all 001

--- tcs_ct_customization/packages/tcs-terraform/default/api-extensions.tf
+++ tcs_ct_customization/packages/tcs-terraform/default/api-extensions.tf
@@ -3,7 +3,7 @@ resource "commercetools_api_extension" "tsc-order-create-extension" {
 
   destination = {
     type = "HTTP"
-    url  = "https://${var.region_functions}-${var.gcp_project_id}.cloudfunctions.net/tscOrderCreate"
+    url  = "https://${var.region_functions}-${var.gcp_project_id}.cloudfunctions.net/tcsOrderCreation"
   }
 
   trigger {
