20ee66ba93a7246e31a8c83e4f7c2558aa381df6
Muhammad Humza Ghayas <humza.ghayas@royalcyber.com>
2023-01-24 13:15:58 +0500

    Changes to apply minimum order quantity

--- tcs-order-create-api-extension/index.js
+++ tcs-order-create-api-extension/index.js
@@ -4,14 +4,25 @@ const functions = require('firebase-functions');
 const tcsOrderCreation = async (req, res) => {
   if (!req.body || !req.body.resource || !req.body.resource) {
     console.log('keep alive call');
-    return res.status(200).json({ result: 'ok' });
+    return res.status(200);
   }
 
   const order = req.body.resource.obj;
 
-  console.log('order', order);
-  console.log('order' + JSON.stringify(order));
-  return res.status(200).json({ result: 'ok1' });
+  if(order.type === 'Order'){
+
+    const sumOfallProdQty = order.customLineItems.map(cli => cli.quantity).reduce((partialSum , q) => partialSum+q,0);
+
+    if(sumOfallProdQty < 4){
+      return res.status(400).json({ 
+        errors: {
+          code:2000,
+          message:"Cart should contain atleast 4 products to be able to checkout!"
+        } 
+      });
+    }
+  }
+  return res.status(200);
 };
 
 module.exports = {
